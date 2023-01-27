d3a6201d49ca02d7f7ddf6e367202ba4b87f50c8
[mick lito] <[micklito.dev@gmail.com]>
2023-01-27 01:51:49 -0500

    Large update -- Index.html changes added in additional time-blocks with realive id tags, updated all textarea classes to have the default css of past.  -- script.js changes added in logic to target clicks to the parent attributes child eq(1)s value to get the input text. made a check to see where the event originated from, which parent div id. then set local storage to the value of the target variable inside of the conditional. created a renderInput function which gets the local storage for each item then targets the text property of text area and sets it to the stored value for each var textarea element. created a headerDate function which inputs the current day threw day.js and assigns the text value of the headerElement to the currrent variable. Finally created a timeColorChanger function which compares the current hour to a interger to decided how to change attributes of the element. if the currentHour is == 18, all text feilds and stored data and colors will be reset to, expected result is a blank form all grayed out. Once current hour reaches 0 or 12am all colors will be set to future, and updated as time goes on. All code need comentation and proper indentation changes, readme update, however all acceptance criteria is met and at a bare level program is ready for deployment.

--- src/script.js
+++ src/script.js
@@ -1,78 +1,188 @@
-$(document).ready(function(){
-
 var buttonEl = $(".btn");
+var HeaderEl = $("#currentDay");
 
-$(function () {
-      buttonEl.on("click", function () {
-      var check = $(this).parent()
-      var check2 = check.children().eq(1).val()
-      localStorage.setItem('hr1', check2)
+$(document).ready(function () {
+  var buttonEl = $(".btn");
 
-      console.log(localStorage.getItem('hr1'))
+  $(function () {
+    buttonEl.on("click", function () {
+      var target = $(this).parent().children().eq(1).val();
+      if ($(this).parent().attr("id") === "hour-9") {
+        localStorage.setItem("hr9", target);
+        console.log("bigfuckingbrainboi");
+      } else if ($(this).parent().attr("id") === "hour-10") {
+        localStorage.setItem("hr10", target);
+        console.log("massivebrainnoodlechamp");
+      } else if ($(this).parent().attr("id") === "hour-11") {
+        localStorage.setItem("hr11", target);
+        console.log("just the biggest fking brains ever");
+      } else if ($(this).parent().attr("id") === "hour-12") {
+        localStorage.setItem("hr12", target);
+        console.log("MONGO LARGE BRAIN BOI");
+      } else if ($(this).parent().attr("id") === "hour-1") {
+        localStorage.setItem("hr1", target);
+      } else if ($(this).parent().attr("id") === "hour-2") {
+        localStorage.setItem("hr2", target);
+      } else if ($(this).parent().attr("id") === "hour-3") {
+        localStorage.setItem("hr3", target);
+      } else if ($(this).parent().attr("id") === "hour-4") {
+        localStorage.setItem("hr4", target);
+      } else if ($(this).parent().attr("id") === "hour-5") {
+        localStorage.setItem("hr5", target);
+      }
+    });
+  });
 
-        $(this).css('background-color', 'red');
-      });
-    }); 
+  $(function renderInput() {
+    var currenthr9 = localStorage.getItem("hr9");
+    buttonEl.parent().children().eq(1).text(currenthr9);
 
-// var hour9 = $("#hour-9");
-// var hour10 = $("#hour-10");
-// var hour11 = $("#hour-11");
+    var currenthr10 = localStorage.getItem("hr10");
+    buttonEl.parent().children().eq(4).text(currenthr10);
 
-// var currentHour = dayjs().format("HH a");
-// console.log(currentHour);
+    var currenthr11 = localStorage.getItem("hr11");
+    buttonEl.parent().children().eq(7).text(currenthr11);
 
-// if (currentHour.includes('pm')) {
-//   if (currentHour > 9) {
-//     hour9.attr("class", "row time-block past");
-//   } else if ((currentHour = 9)) {
-//     hour9.attr("class", "row time-block present");
-//   } else if (currentHour < 9) {
-//     hour9.attr("class", "row time-block future");
-//   }
+    var currenthr12 = localStorage.getItem("hr12");
+    buttonEl.parent().children().eq(10).text(currenthr12);
 
-//   if (currentHour > 10) {
-//     hour10.attr("class", "row time-block past");
-//   } else if ((currentHour = 10)) {
-//     hour10.attr("class", "row time-block present");
-//   } else if (currentHour < 10) {
-//     hour10.attr("class", "row time-block future");
-//   }
+    var currenthr1 = localStorage.getItem("hr1");
+    buttonEl.parent().children().eq(13).text(currenthr1);
 
-//   if (currentHour > 11) {
-//     hour11.attr("class", "row time-block past");
-//   } else if ((currentHour = 11)) {
-//     hour11.attr("class", "row time-block present");
-//   } else if (currentHour < 11) {
-//     hour11.attr("class", "row time-block future");
-//   }
-// }
+    var currenthr2 = localStorage.getItem("hr2");
+    buttonEl.parent().children().eq(16).text(currenthr2);
 
+    var currenthr3 = localStorage.getItem("hr3");
+    buttonEl.parent().children().eq(19).text(currenthr3);
 
-//    $('#hour-9').text(localStorage.getItem(''));
+    var currenthr4 = localStorage.getItem("hr4");
+    buttonEl.parent().children().eq(22).text(currenthr4);
 
+    var currenthr5 = localStorage.getItem("hr5");
+    buttonEl.parent().children().eq(25).text(currenthr5);
+  });
 
-})
+  $(function headerDate() {
+    var currentDay = dayjs().format("M /DD /YYYY");
+    HeaderEl.text(currentDay);
+  });
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
+  $(function timeColorChanger() {
+    setInterval(function () {
+      var currentHour = dayjs().format("H");
+      // currentHour = 10
+      var hour9 = $("#hour-9");
+      var hour10 = $("#hour-10");
+      var hour11 = $("#hour-11");
+      var hour12 = $("#hour-12");
+      var hour1 = $("#hour-1");
+      var hour2 = $("#hour-2");
+      var hour3 = $("#hour-3");
+      var hour4 = $("#hour-4");
+      var hour5 = $("#hour-5");
 
+      if (currentHour > 9) {
+        hour9.attr("class", "row time-block past");
+      } else if (currentHour == 9) {
+        hour9.attr("class", "row time-block present");
+      } else if (currentHour < 9 && currentHour >= 0) {
+        hour9.attr("class", "row time-block future");
+      }
 
+      if (currentHour > 10) {
+        hour10.attr("class", "row time-block past");
+      } else if (currentHour == 10) {
+        hour10.attr("class", "row time-block present");
+      } else if (currentHour < 10 && currentHour >= 0) {
+        hour10.attr("class", "row time-block future");
+      }
 
+      if (currentHour > 11) {
+        hour11.attr("class", "row time-block past");
+      } else if (currentHour == 11) {
+        hour11.attr("class", "row time-block present");
+      } else if (currentHour < 11 && currentHour >= 0) {
+        hour11.attr("class", "row time-block future");
+      }
 
+      if (currentHour > 12) {
+        hour12.attr("class", "row time-block past");
+      } else if (currentHour == 12) {
+        hour12.attr("class", "row time-block present");
+      } else if (currentHour < 12 && currentHour >= 0) {
+        hour12.attr("class", "row time-block future");
+      }
 
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
+      if (currentHour > 13) {
+        hour1.attr("class", "row time-block past");
+      } else if (currentHour == 13) {
+        hour1.attr("class", "row time-block present");
+      } else if (currentHour < 13 && currentHour >= 0) {
+        hour1.attr("class", "row time-block future");
+      }
+
+      if (currentHour > 14) {
+        hour2.attr("class", "row time-block past");
+      } else if (currentHour == 14) {
+        hour2.attr("class", "row time-block present");
+      } else if (currentHour < 14 && currentHour >= 0) {
+        hour2.attr("class", "row time-block future");
+      }
+
+      if (currentHour > 15) {
+        hour3.attr("class", "row time-block past");
+      } else if (currentHour == 15) {
+        hour3.attr("class", "row time-block present");
+      } else if (currentHour < 15 && currentHour >= 0) {
+        hour3.attr("class", "row time-block future");
+      }
+
+      if (currentHour > 16) {
+        hour4.attr("class", "row time-block past");
+      } else if (currentHour == 16) {
+        hour4.attr("class", "row time-block present");
+      } else if (currentHour < 16 && currentHour >= 0) {
+        hour4.attr("class", "row time-block future");
+      }
+
+      if (currentHour > 17) {
+        hour5.attr("class", "row time-block past");
+      } else if (currentHour == 17) {
+        hour5.attr("class", "row time-block present");
+      } else if (currentHour < 17 && currentHour >= 0) {
+        hour5.attr("class", "row time-block future");
+      }
+
+      if (currentHour == 18) {
+        var hours = [
+          hour9,
+          hour10,
+          hour11,
+          hour12,
+          hour1,
+          hour2,
+          hour3,
+          hour4,
+          hour5,
+        ];
+        var hr = [
+          "hr9",
+          "hr10",
+          "hr11",
+          "hr12",
+          "hr1",
+          "hr2",
+          "hr3",
+          "hr4",
+          "hr5",
+        ];
+        for (const hour of hours) {
+          hour.children().eq(1).text(" ");
+        }
+
+        for (const element of hr) localStorage.setItem(`${element}`, "");
+      }
+    }, 100);
+  });
+});
+
